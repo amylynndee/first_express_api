7599d993c6a0672422afdc169507e81a244247cd
amylynndee <amy.damjanovich@gmail.com>
2022-12-04 22:21:27 -0600

    init

---
+++ hello-vue/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    devServer: {
+        proxy: 'http://127.0.0.1:3000'
+    }
+}
+
+
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   devServer: {
+//     proxy: 'http://127.0.0.1:3000'
+//   }
+// })
